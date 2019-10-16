db37e74fb728773e88ae42743ce0664b4db1df9f
Eric <e.lim0322@gmail.com>
2019-10-17 09:50:30 +1300

    change license header for attestation

--- prml/attestation/src/lib.rs
+++ prml/attestation/src/lib.rs
@@ -1,5 +1,5 @@
-// Copyright (C) 2019 Centrality Investments Limited
-// This file is part of CENNZnet.
+// Copyright (C) 2019 Plug New Zealand Limited
+// This file is part of Plug.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
